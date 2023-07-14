b4460a4431483797044c0b73dac06761bd1185cd
Railson OL <itsrayplay@gmail.com>
2023-07-14 11:06:45 -0300

    Update attribuition.tsx

--- src/app/frontendmentor/attribuition.tsx
+++ src/app/frontendmentor/attribuition.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Attribution() {
   return (
-    <div className="attribution">
+    <footer className="attribution">
       Challenge by{' '}
       <Link href={'https://www.frontendmentor.io?ref=challenge'}>
         Frontend Mentor
@@ -11,6 +11,6 @@ export default function Attribution() {
       <Link href={'https://github.com/RailsonOL?tab=repositories'}>
         Zerohora (RailsonOL)
       </Link>
-    </div>
+    </footer>
   )
 }
