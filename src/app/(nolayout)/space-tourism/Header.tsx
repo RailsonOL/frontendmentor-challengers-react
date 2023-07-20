d2e9654c2a32b8797241683b0113c80725e736fd
Railson OL <itsrayplay@gmail.com>
2023-07-19 21:50:24 -0300

    working

--- src/app/(nolayout)/space-tourism/Header.tsx
+++ src/app/(nolayout)/space-tourism/Header.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
+import Link from 'next/link'
 import { useState } from 'react'
 import './style.css'
 
-export default function Header() {
+interface Props {
+  currentPage: number
+}
+
+export default function Header({ currentPage = 1 }: Props) {
   const [isOpen, setIsOpen] = useState(false)
 
   const handleClick = () => {
@@ -41,25 +46,41 @@ export default function Header() {
           } absolute right-0 top-0 z-20 flex flex-col bg-white/5 tracking-[0.15rem] backdrop-blur-lg max-sm:h-screen max-sm:w-2/3 max-sm:px-10 max-sm:py-32 sm:static sm:flex md:w-full md:justify-center`}
         >
           <ul className="h-full items-center justify-center gap-10 max-sm:space-y-6 sm:flex lg:pr-32">
-            <li className="flex flex-col border-b-2 border-b-white hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2">
-              <a href="#">
+            <li
+              className={`flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2 ${
+                currentPage === 1 ? 'border-b-2 border-b-white' : ''
+              }`}
+            >
+              <Link href="/space-tourism">
                 <span className="mr-1 font-bold md:hidden lg:inline">00</span>
                 Home
-              </a>
+              </Link>
             </li>
-            <li className="flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2">
-              <a href="#">
+            <li
+              className={`flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2 ${
+                currentPage === 2 ? 'border-b-2 border-b-white' : ''
+              }`}
+            >
+              <Link href="/space-tourism/destination">
                 <span className="mr-1 font-bold md:hidden lg:inline">01</span>{' '}
                 Destination
-              </a>
+              </Link>
             </li>
-            <li className="flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2">
+            <li
+              className={`flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2 ${
+                currentPage === 3 ? 'border-b-2 border-b-white' : ''
+              }`}
+            >
               <a href="#">
                 <span className="mr-1 font-bold md:hidden lg:inline">02</span>{' '}
                 Crew
               </a>
             </li>
-            <li className="flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2">
+            <li
+              className={`flex flex-col hover:border-b-white/50 md:h-full md:items-center md:justify-center md:hover:border-b-2 ${
+                currentPage === 4 ? 'border-b-2 border-b-white' : ''
+              }`}
+            >
               <a href="#">
                 <span className="mr-1 font-bold md:hidden lg:inline">03</span>{' '}
                 Technology
--- src/app/(nolayout)/space-tourism/page.tsx
+++ src/app/(nolayout)/space-tourism/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import Header from './Header'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col md:gap-10">
-      <Header />
+    <div className="container-home flex min-h-screen flex-col md:gap-10">
+      <Header currentPage={1} />
       <main className="flex items-center gap-16 p-6 text-center max-lg:flex-col md:gap-44 lg:mb-36 lg:mt-auto lg:px-40">
         <div className="max-w-[27.75rem] text-center lg:text-left">
           <h5>So, you want to travel to</h5>
@@ -19,7 +20,7 @@ export default function Home() {
 
         <div className="lg:ml-auto lg:self-end">
           <button className="group relative h-[9.375rem] w-[9.375rem] rounded-full bg-white font-['Bellefair'] text-[1.25rem] uppercase text-gray-950 sm:h-[17.125rem] sm:w-[17.125rem] sm:text-[2rem]">
-            <a href="#">Explore</a>
+            <Link href="/space-tourism/destination">Explore</Link>
             <div className="absolute left-0 right-0 top-0 -z-10 h-[9.375rem] w-[9.375rem] rounded-full bg-white/50 duration-100 group-hover:scale-150 sm:h-[17.125rem] sm:w-[17.125rem]"></div>
           </button>
         </div>
